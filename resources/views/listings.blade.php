2f0e95172a9c4612d5996c671ede1ec243c0679e
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-17 19:00:51 +0100

    index page modified

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,10 +1,15 @@
 <h1>{{ $heading }}</h1>
 
-@if(count($listings) == 0)
+{{-- @if(count($listings) == 0)
     <p>No listings</p>
-@endif
+@endif --}}
 
+@unless(count($listings) == 0)
 @foreach($listings as $listing)
 <h2>{{ $listing['title'] }}</h2>
 <p>{{ $listing['description'] }}</p>
 @endforeach
+
+@else
+<p>No Listings</p>
+@endunless
--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,22 @@ Route::get('/', function () {
     return view('listings', [
         'heading' => 'Latest Listing',
         'listings' => [
-            // [
-            //     'id' => 1,
-            //     'title' => 'Listing One',
-            //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            //                         Fuga libero consectetur consequuntur ipsa eveniet atque numquam aliquam
-            //                         sed ipsam maiores pariatur cumque laborum nemo tenetur animi,
-            //                         quis veniam odio quae?',
-            // ],
-            // [
-            //     'id' => 2,
-            //     'title' => 'Listing Two',
-            //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            //                         Fuga libero consectetur consequuntur ipsa eveniet atque numquam aliquam
-            //                         sed ipsam maiores pariatur cumque laborum nemo tenetur animi,
-            //                         quis veniam odio quae?',
-            // ],
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Fuga libero consectetur consequuntur ipsa eveniet atque numquam aliquam
+                                    sed ipsam maiores pariatur cumque laborum nemo tenetur animi,
+                                    quis veniam odio quae?',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Fuga libero consectetur consequuntur ipsa eveniet atque numquam aliquam
+                                    sed ipsam maiores pariatur cumque laborum nemo tenetur animi,
+                                    quis veniam odio quae?',
+            ],
         ]
     ]);
 });
