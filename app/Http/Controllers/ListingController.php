d1522376b066d96135eae012d940df74dad93835
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-19 19:51:27 +0100

    scope filtering

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,10 +9,8 @@ class ListingController extends Controller
 {
     //show all listings
     public function index(){
-        dd(request('tag'));
-
         return view('listings.index', [
-            'listings' => Listing::all(),
+            'listings' => Listing::latest()->filter(request(['tag']))->get(),
         ]);
     }
 
