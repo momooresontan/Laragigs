f367797f93deb247a3d098143f4502ce61b706aa
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-01-21 08:50:33 +0100

    paginate

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -5,8 +5,9 @@
     <textarea
         class="border border-gray-200 rounded p-2 w-full"
         name="{{ $name }}"
-        {{ $attributes }}
-    >{{ $slot ?? old($name) }}</textarea>
+        {{ $attributes }}>
+        {{ old($name) }}
+    </textarea>
 
     <x-form.error name="{{ $name }}" />
 </x-form.field>
